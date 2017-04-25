ded20ebeca8b4a9a97a7481a16cfba982b414030
Beniamin Makal <beniamin.makal@gmail.com>
2017-04-25 15:40:43 +0200

    Add array to "Sale" form.

--- Cient App for Warehouse/Sale.cs
+++ Cient App for Warehouse/Sale.cs
@@ -60,7 +60,7 @@ namespace Cient_App_for_Warehouse
                 int price = 0;
                 newCon a = new newCon(Logon.log, Logon.pas);
                 price = a.GetPrice(ref P_name);
-
+                string[] arr = new string[6];
 
 
                 if (price != 0)
